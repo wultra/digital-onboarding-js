b34b94f8a1f0d84f2d7c07ff7709c9f4112090b8
Jan Kobersky <kober32@gmail.com>
2026-01-30 17:00:38 +0100

    Fixed test for onboarding-approval

--- testapp-cordova/src/demodata.ts
+++ testapp-cordova/src/demodata.ts
@@ -1,7 +1,8 @@
 // Server configuration
 export const serverCredentials = {
-    server: "",
-    paConfig: "",
+    server: "", // e.g. "https://powerath-server-example.com"
+    paConfig: "", // BASe64-encoded PowerAuth configuration
+    authorization: "" // Base64-encoded Basic Auth credentials for the server API (just the part after "Basic ")
 }
 
 export const blinkIdIos = ""
